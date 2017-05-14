d7c221467d683f89dc493a147531a8ca6317a4a1
Brian <tzhlee@gmail.com>
2017-05-14 10:00:12 -0500

    2nd version of React Boiler Plate

---
+++ app/components/Main.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+
+/*var Main = React.createClass({
+    render: function() {
+        return(
+            <div>
+                <Nav/>
+                <h2>Main Component</h2>
+                {this.props.children}
+            </div>
+        );
+    }
+}); */
+
+var Main = (props) => {
+    return(
+        <div>
+            <div>
+                <div>
+				<p>Main.jsx Rendered</p>
+                    {props.children}
+                 </div>
+            </div>
+        </div>
+    );
+}
+
+module.exports = Main;
